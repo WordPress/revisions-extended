0b3e97c6ce8db10cdc07fd3f04e7bbbe199a740e
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2021-01-12 15:14:50 -0800

    Initialize the rest routes

--- revisions-extended/index.php
+++ revisions-extended/index.php
@@ -10,8 +10,13 @@
 
 namespace RevisionsExtended;
 
+use RevisionsExtended\REST_Revisions_Controller;
+
 defined( 'WPINC' ) || die();
 
+/**
+ * Constants.
+ */
 define( __NAMESPACE__ . '\PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( __NAMESPACE__ . '\PLUGIN_URL', plugins_url( '/', __FILE__ ) );
 
@@ -19,6 +24,7 @@ define( __NAMESPACE__ . '\PLUGIN_URL', plugins_url( '/', __FILE__ ) );
  * Actions and filters.
  */
 add_action( 'plugins_loaded', __NAMESPACE__ . '\load_files' );
+add_action( 'rest_api_init', __NAMESPACE__ . '\initialize_rest_routes', 100 );
 
 /**
  * Load the other PHP files for the plugin.
@@ -38,4 +44,18 @@ function load_files() {
  */
 function get_includes_path() {
 	return PLUGIN_DIR . 'includes/';
+}
+
+/**
+ * Create REST controllers and register their routes.
+ *
+ * @return void
+ */
+function initialize_rest_routes() {
+	foreach ( get_post_types( array( 'show_in_rest' => true ), 'objects' ) as $post_type ) {
+		if ( post_type_supports( $post_type->name, 'revisions' ) ) {
+			$controller = new REST_Revisions_Controller( $post_type->name );
+			$controller->register_routes();
+		}
+	}
 }
