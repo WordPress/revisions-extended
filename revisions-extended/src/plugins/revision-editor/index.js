8e8bba876854c2fd8e0a4a04f75ec1ce6a4a295a
dufresnesteven <steve.dufresne@automattic.com>
2021-01-26 15:21:14 +0900

    Remove the plugin sidebar.

--- revisions-extended/src/plugins/revision-editor/index.js
+++ revisions-extended/src/plugins/revision-editor/index.js
@@ -7,13 +7,8 @@ import { registerPlugin } from '@wordpress/plugins';
  * Internal dependencies
  */
 import DocumentSettingPanel from './document-setting-panel';
-import PluginSidebar from './plugin-sidebar';
 import { pluginNamespace } from '../../utils';
 
 registerPlugin( `${ pluginNamespace }-document-setting-panel`, {
 	render: DocumentSettingPanel,
-} );
-
-registerPlugin( `${ pluginNamespace }-plugin-sidebar`, {
-	render: PluginSidebar,
 } );
