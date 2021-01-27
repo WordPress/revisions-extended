74b1646ba7b56e9a0703f9c9aded3935c24f88ea
dufresnesteven <steve.dufresne@automattic.com>
2021-01-27 10:00:33 +0900

    Kill the delete revision.

--- revisions-extended/src/plugins/revision-editor/document-setting-panel/components/index.js
+++ revisions-extended/src/plugins/revision-editor/document-setting-panel/components/index.js
@@ -1,5 +1,4 @@
-import DeleteRevision from './delete-revision';
 import PublishRevision from './publish-revision';
 import Revisions from './revisions';
 
-export { DeleteRevision, PublishRevision, Revisions };
+export { PublishRevision, Revisions };
