d27c43f5239d0052352f65c83954d53a3feea80d
dufresnesteven <steve.dufresne@automattic.com>
2021-01-28 11:27:25 +0900

    Udpate the revision banner.

--- revisions-extended/src/plugins/revision-editor/revision-indicator/index.js
+++ revisions-extended/src/plugins/revision-editor/revision-indicator/index.js
@@ -17,16 +17,18 @@ import { usePost } from '../../../hooks';
 const RevisionIndicator = () => {
 	const { savedPost } = usePost();
 
+	const notes = [
+		'You are currently editing a <b>scheduled revision</b>.',
+		`[ <a href="/wp-admin/post.php?post=${ savedPost.parent }&action=edit">View post</a>`,
+		` | <a href="/wp-admin/revision.php?revision=${ savedPost.id }&gutenberg=true" />See changes</a> ]`,
+	];
+
 	useEffect( () => {
-		dispatch( 'core/notices' ).createNotice(
-			'warning',
-			`You are currently editing a <b>scheduled revision</b>. <a href="/wp-admin/post.php?post=${ savedPost.parent }&action=edit">View original post</a>.`,
-			{
-				__unstableHTML: true,
-				id: 'revisions-extended-notice',
-				isDismissible: false,
-			}
-		);
+		dispatch( 'core/notices' ).createNotice( 'warning', notes.join( ' ' ), {
+			__unstableHTML: true,
+			id: 'revisions-extended-notice',
+			isDismissible: false,
+		} );
 	}, [] );
 
 	return null;
