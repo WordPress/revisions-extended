d6e9ec22687bcaffd68fdaaa9bc8e7c388ba70a2
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2021-01-27 16:56:12 -0800

    Switch to built in post stati

--- revisions-extended/includes/post-status.php
+++ revisions-extended/includes/post-status.php
@@ -5,78 +5,23 @@ namespace RevisionsExtended\Post_Status;
 defined( 'WPINC' ) || die();
 
 /**
- * Actions and filters.
- */
-add_action( 'init', __NAMESPACE__ . '\register' );
-
-/**
- * Register post statuses.
- *
- * @return void
- */
-function register() {
-	register_post_status(
-		'revex_pending',
-		array(
-			'label'                     => __( 'Pending Revision', 'revisions-extended' ),
-			'labels'                    => (object) array(
-				'publish' => __( 'Publish Revision', 'revisions-extended' ),
-				'save'    => __( 'Save Revision', 'revisions-extended' ),
-				'update'  => __( 'Update Revision', 'revisions-extended' ),
-				'plural'  => __( 'Pending Revisions', 'revisions-extended' ),
-				'short'   => __( 'Pending', 'revisions-extended' )
-			),
-			'protected'                 => true,
-			'internal'                  => true,
-			'label_count'               => _n_noop(
-				'Pending <span class="count">(%s)</span>',
-				'Pending <span class="count">(%s)</span>',
-				'revisions-extended'
-			),
-			'exclude_from_search'       => false,
-			'show_in_admin_all_list'    => false,
-			'show_in_admin_status_list' => false,
-			'revisions_extended'        => true,
-		)
-	);
-
-	register_post_status(
-		'revex_future',
-		array(
-			'label'                     => __( 'Scheduled Revision', 'revisions-extended' ),
-			'labels'                    => (object) array(
-				'publish' => __( 'Publish Revision', 'revisions-extended' ),
-				'save'    => __( 'Save Revision', 'revisions-extended' ),
-				'update'  => __( 'Update Revision', 'revisions-extended' ),
-				'plural'  => __( 'Scheduled Revisions', 'revisions-extended' ),
-				'short'   => __( 'Scheduled', 'revisions-extended' )
-			),
-			'protected'                 => true,
-			'internal'                  => true,
-			'label_count'               => _n_noop(
-				'Scheduled <span class="count">(%s)</span>',
-				'Scheduled <span class="count">(%s)</span>',
-				'revisions-extended'
-			),
-			'exclude_from_search'       => false,
-			'show_in_admin_all_list'    => false,
-			'show_in_admin_status_list' => false,
-			'revisions_extended'        => true,
-		)
-	);
-}
-
-/**
  * Get array of revision-specific post status objects.
  *
  * @return object[]
  */
 function get_revision_statuses() {
-	return get_post_stati(
+	$stati = get_post_stati(
 		array(
-			'revisions_extended' => true,
+			'_builtin' => true,
 		),
 		'objects'
+	);
+
+	return array_intersect_key(
+		$stati,
+		array(
+			'future' => true,
+		)
 	);
 }
 
