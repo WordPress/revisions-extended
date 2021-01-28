34755de6642e04e565d2527d4b938ef5ee35b8c6
dufresnesteven <steve.dufresne@automattic.com>
2021-01-28 09:33:52 +0900

    Update the test for the revision banner.

--- revisions-extended/src/plugins/revision-editor/revision-indicator/index.js
+++ revisions-extended/src/plugins/revision-editor/revision-indicator/index.js
@@ -21,7 +21,7 @@ const RevisionIndicator = () => {
 		const [ id ] = savedPost.slug.split( '-' );
 		dispatch( 'core/notices' ).createNotice(
 			'warning',
-			`You are currently editing a <u>scheduled revision</u> for post #<a href="/wp-admin/post.php?post=${ id }&action=edit">${ id }</a>.`,
+			`You are currently editing a <b>scheduled revision</b>. <a href="/wp-admin/post.php?post=${ id }&action=edit">View original post</a>.`,
 			{
 				__unstableHTML: true,
 				id: 'revisions-extended-notice',
