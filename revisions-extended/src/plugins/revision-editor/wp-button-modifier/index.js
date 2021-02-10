66f556a4753c2a55c7b8ff55a2ca562581b20fde
Steven Dufresne <steve.dufresne@automattic.com>
2021-02-10 13:59:01 +0900

    Update link on WP Button (#38)

---
+++ revisions-extended/src/plugins/revision-editor/wp-button-modifier/index.js
@@ -0,0 +1,43 @@
+/**
+ * WordPress dependencies
+ */
+import { useEffect } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import { useParentPost } from '../../../hooks';
+import { getAllRevisionUrl } from '../../../utils';
+
+/**
+ * Module variables
+ */
+let wpButtonElement;
+
+/**
+ * Returns the WP button element
+ *
+ * @return {HTMLElement} Html anchor tag.
+ */
+const getWPButton = () => {
+	if ( wpButtonElement ) return wpButtonElement;
+
+	return document.querySelector( '.edit-post-fullscreen-mode-close' );
+};
+
+const WPButtonModifier = () => {
+	const { type } = useParentPost();
+
+	useEffect( () => {
+		if ( ! type ) return;
+		const btn = getWPButton();
+
+		if ( btn ) {
+			btn.href = getAllRevisionUrl( type );
+		}
+	} );
+
+	return null;
+};
+
+export default WPButtonModifier;
