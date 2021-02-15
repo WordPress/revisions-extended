fdfb1af7072c862a4f24ecf84adb8ccc93c6307f
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2021-02-15 15:29:52 -0800

    Unschedule publish events when revisions are deleted

--- revisions-extended/includes/cron.php
+++ revisions-extended/includes/cron.php
@@ -11,6 +11,8 @@ defined( 'WPINC' ) || die();
  */
 add_action( 'future_revision', __NAMESPACE__ . '\unschedule_default_publish', 20 );
 add_action( 'future_revision', __NAMESPACE__ . '\schedule_update', 30, 2 );
+add_action( 'wp_delete_post_revision', __NAMESPACE__ . '\unschedule_update', 10, 2 );
+add_action( 'rest_delete_revision', __NAMESPACE__ . '\unschedule_update', 10, 2 );
 
 /**
  * Remove the default scheduled event that would attempt to publish the future revision post.
@@ -35,4 +37,28 @@ function schedule_update( $post_id, $post ) {
 	$time = strtotime( $post->post_date_gmt . ' GMT' );
 
 	wp_schedule_single_event( $time, 'publish_future_revision', array( $post->ID ) );
+}
+
+/**
+ * Unschedule a future event to update a parent post with a revision when the revision is deleted.
+ *
+ * @param int|WP_Post $revision
+ * @param WP_Post|\WP_REST_Request
+ */
+function unschedule_update( $revision, $revision2 ) {
+	$update = null;
+
+	if ( $revision instanceof WP_Post ) {
+		$update = $revision;
+	} elseif ( $revision2 instanceof WP_Post ) {
+		$update = $revision2;
+	}
+
+	if ( ! $update ) {
+		return;
+	}
+
+	if ( 'future' === get_post_status( $update ) ) {
+		wp_clear_scheduled_hook( 'publish_future_revision', array( $update->ID ) );
+	}
 }
