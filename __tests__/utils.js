94e2a80cee1a9f3f7e081c6114c2641dc4457f39
Steven Dufresne <steve.dufresne@automattic.com>
2021-02-15 15:34:26 +0900

    Update the paths using the wordpress url component. (#42)

---
+++ __tests__/utils.js
@@ -0,0 +1,26 @@
+/**
+ * Internal dependencies
+ */
+import { getAllRevisionUrl, getEditUrl } from '../revisions-extended/src/utils';
+
+describe( 'Utils', () => {
+	it( 'Should return an empty string if no type is passes', () => {
+		expect( getAllRevisionUrl() ).toEqual( '' );
+	} );
+
+	it( 'Should return post revision list', () => {
+		expect( getAllRevisionUrl( 'post' ) ).toEqual(
+			'edit.php?page=post-updates'
+		);
+	} );
+
+	it( 'Should return custom post type revision list', () => {
+		expect( getAllRevisionUrl( 'custom' ) ).toEqual(
+			'edit.php?post_type=custom&page=custom-updates'
+		);
+	} );
+
+	it( 'Should return full formed edit url', () => {
+		expect( getEditUrl( 1 ) ).toEqual( 'post.php?post=1&action=edit' );
+	} );
+} );
