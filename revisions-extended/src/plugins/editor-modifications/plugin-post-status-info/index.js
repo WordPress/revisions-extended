1d4f6504591df43bb38845fc255844764e1fb139
dufresnesteven <steve.dufresne@automattic.com>
2021-02-16 13:53:07 +0900

    Autocheck the create checkbox when the future date is chosen.

--- revisions-extended/src/plugins/editor-modifications/plugin-post-status-info/index.js
+++ revisions-extended/src/plugins/editor-modifications/plugin-post-status-info/index.js
@@ -1,6 +1,7 @@
 /**
  * WordPress dependencies
  */
+import { useEffect } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { PluginPostStatusInfo as PostStatusInfo } from '@wordpress/edit-post';
 import { CheckboxControl } from '@wordpress/components';
@@ -8,10 +9,21 @@ import { CheckboxControl } from '@wordpress/components';
 /**
  * Internal dependencies
  */
-import { useInterface } from '../../../hooks';
+import { useInterface, usePost } from '../../../hooks';
 
 const PluginPostStatusInfo = () => {
 	const { shouldIntercept, setShouldIntercept } = useInterface();
+	const { changingToScheduled } = usePost();
+
+	const isChangingToScheduled = changingToScheduled();
+
+	useEffect( () => {
+
+        // We want to update the checkbox if the date is moving into the future.
+		if ( shouldIntercept !== isChangingToScheduled ) {
+			setShouldIntercept( isChangingToScheduled );
+		}
+	}, [ isChangingToScheduled ] );
 
 	return (
 		<PostStatusInfo>
