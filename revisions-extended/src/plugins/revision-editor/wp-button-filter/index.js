83c0d84ea160c291c0b8616c30ec886385ead4db
dufresnesteven <steve.dufresne@automattic.com>
2021-02-17 11:35:48 +0900

    Change the button text on the revision editor.

---
+++ revisions-extended/src/plugins/revision-editor/wp-button-filter/index.js
@@ -0,0 +1,24 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { addFilter } from '@wordpress/hooks';
+
+/**
+ * Updates the button text for the revision page
+ */
+addFilter(
+	'i18n.gettext',
+	'revision-extended/update-btn-text',
+	( translation, text ) => {
+		if ( text === 'Schedule' ) {
+			return __( 'Update', 'revisions-extended' );
+		}
+
+		if ( text === 'Scheduling…' ) {
+			return __( 'Update…', 'revisions-extended' );
+		}
+
+		return translation;
+	}
+);
